327ed20d73100159b99e55965e82afd054f26362
harshag14 <harshag1410@gmail.com>
2020-11-02 18:56:32 +0530

    hello

--- Hotzone/phase1_config/settings.py
+++ Hotzone/phase1_config/settings.py
@@ -25,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mrvpql!jvdvk@uzcpz(6%8oe%(&mp=eitm+9i)br7^z&pru04*'
+#SECRET_KEY = 'mrvpql!jvdvk@uzcpz(6%8oe%(&mp=eitm+9i)br7^z&pru04*'
+SECRET_KEY = env('HOTZONE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['hotzone7.herokuapp.com', 'localhost', '127.0.0.1']
 
